40060481e493dd73a954e55a52ecb7dd539c5d4a
Tom Moulard <tom.moulard@epita.fr>
2019-09-18 15:18:15 +0200

    umanagement: fixed using data.Data fixing Salt

--- api/umanagement/umanagement.go
+++ api/umanagement/umanagement.go
@@ -1,8 +1,6 @@
 package umanagement
 
 import (
-	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -10,11 +8,10 @@ import (
 	"os"
 	"time"
 
+	"eiko/misc/data"
 	"eiko/misc/hash"
 	"eiko/misc/misc"
 	"eiko/misc/structures"
-
-	"cloud.google.com/go/datastore"
 )
 
 var (
@@ -23,70 +20,46 @@ var (
 		log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Salt salt for the passwrd hashing
-	Salt = "tmp"
-
-	// Users users name inside the datastore
-	Users = "Users"
+	Salt = hash.GenerateKey(666)
 )
-
-// ParseJSON generic function to parse request body, extract it's content and
-// fill the struct
-func ParseJSON(r *http.Request, v interface{}) error {
-	decoder := json.NewDecoder(r.Body)
-	defer r.Body.Close()
-	err := decoder.Decode(v)
-	if err != nil {
-		Logger.Printf("\033[31mError\033[0m: '%s'\n", err.Error())
-	}
-	return err
-}
 
 // Login get the Login informations and return the token to the user if the
 // credentials are valid
-func Login(ctx context.Context, r *http.Request,
-	client *datastore.Client) (string, error) {
+func Login(d data.Data, r *http.Request) (string, error) {
 	var i structures.Login
-	err := ParseJSON(r, &i)
+	err := misc.ParseJSON(r, &i)
 	if err != nil {
 		return "", errors.New("1.0.0")
 	}
-	var users []structures.User
-	q := datastore.NewQuery(Users).Filter("Email =", i.UserMail).Limit(1)
-	if _, err := client.GetAll(ctx, q, &users); err != nil {
-		Logger.Printf("%q", err)
+
+	user, err := d.GetUser(i.UserMail)
+	if err != nil {
 		return "", errors.New("1.0.1")
 	}
-	if len(users) == 0 {
-		Logger.Println("no user found")
+
+	err = hash.CompareHash(user.Pass, i.UserPass, Salt)
+	if err != nil {
 		return "", errors.New("1.0.2")
 	}
-	User := users[0]
-	Logger.Printf("%v\n", User)
 
-	err = hash.CompareHash(User.Pass, i.UserPass, Salt)
+	token, err := misc.UserToToken(user)
 	if err != nil {
 		return "", errors.New("1.0.3")
-	}
-
-	token, err := misc.UserToToken(User)
-	if err != nil {
-		return "", errors.New("1.0.4")
 	}
 
 	return fmt.Sprintf("{\"token\":\"%s\"}", token), nil
 }
 
 // Register adds a new user to the datastore if the credentials are valid
-func Register(ctx context.Context, r *http.Request,
-	client *datastore.Client) (string, error) {
+func Register(d data.Data, r *http.Request) (string, error) {
 	var i structures.Login
-	err := ParseJSON(r, &i)
+	err := misc.ParseJSON(r, &i)
 	if err != nil {
 		return "", errors.New("1.1.0")
 	}
 
-	if err = misc.UniqEmail(ctx, Users, i.UserMail, client); err != nil {
-		return "", errors.New("1.1.1\", \"message\":\"not a unique email")
+	if _, err = d.GetUser(i.UserMail); err == nil {
+		return "", errors.New("1.1.1")
 	}
 
 	pass, err := hash.Hash(i.UserPass, Salt)
@@ -94,20 +67,18 @@ func Register(ctx context.Context, r *http.Request,
 		return "", errors.New("1.1.2")
 	}
 
-	User := structures.User{
+	user := structures.User{
 		Email:     i.UserMail,
 		Pass:      pass,
 		Created:   time.Now(),
 		Validated: false,
 	}
-	key := datastore.IncompleteKey(Users, nil)
-	_, err = client.Put(ctx, key, &User)
-	if err != nil {
+	if d.StoreUser(user); err != nil {
 		Logger.Println(err)
 		return "", errors.New("1.1.3")
 	}
 
-	token, err := misc.UserToToken(User)
+	token, err := misc.UserToToken(user)
 	if err != nil {
 		return "", errors.New("1.1.4")
 	}
@@ -116,10 +87,9 @@ func Register(ctx context.Context, r *http.Request,
 }
 
 // Delete delete an account
-func Delete(ctx context.Context, r *http.Request,
-	client *datastore.Client) (string, error) {
+func Delete(d data.Data, r *http.Request) (string, error) {
 	var i structures.Token
-	err := ParseJSON(r, &i)
+	err := misc.ParseJSON(r, &i)
 	if err != nil {
 		return "", errors.New("1.2.0")
 	}
@@ -134,10 +104,9 @@ func Delete(ctx context.Context, r *http.Request,
 }
 
 // UpdateToken delete an account
-func UpdateToken(ctx context.Context, r *http.Request,
-	client *datastore.Client) (string, error) {
+func UpdateToken(d data.Data, r *http.Request) (string, error) {
 	var i structures.Token
-	err := ParseJSON(r, &i)
+	err := misc.ParseJSON(r, &i)
 	if err != nil {
 		return "", errors.New("1.3.0")
 	}
