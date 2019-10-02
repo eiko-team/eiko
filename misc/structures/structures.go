5f93f0f726fde2dc4f438d2053d8575a6750d6f1
Tom Moulard <tom.moulard@epita.fr>
2019-10-02 15:34:04 +0200

    consumable: moving field Mode from consumable to ListContent

--- misc/structures/structures.go
+++ misc/structures/structures.go
@@ -109,13 +109,6 @@ type Consumable struct {
 	Pictures        Pictures        `json:"pictures"`
 	Quantity        Quantity        `json:"quantity"`
 	ID              int64           // The integer ID used in the firestore.
-	// Mode is used to signifiy the mode of the consumable
-	// It's content should be:
-	// 	- "sample": for testing purpose
-	// 	- "consumable": for a "real" consumable
-	// 	- "personnal": for a user imported consmable where only the Name field is
-	// used. stock and store are also not used
-	Mode string `json:"mode"`
 }
 
 // Stock Stock of a product in a store
@@ -167,4 +160,11 @@ type ListContent struct {
 	Name        string      `json:"name" firestore:"name"`
 	Done        bool        `json:"done" firestore:"done"`
 	Erased      bool        `json:"erased" firestore:"erased"`
+	// Mode is used to signifiy the mode of the consumable
+	// It's content should be:
+	// 	- "sample": for testing purpose
+	// 	- "consumable": for a "real" consumable
+	// 	- "personnal": for a user imported consmable where only the Name field is
+	// used. stock and store are also not used
+	Mode string `json:"mode"`
 }
