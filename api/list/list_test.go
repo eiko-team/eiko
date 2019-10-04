5af8f87bdbf6b580cb9e3562b26476958d9814f5
Tom Moulard <tom.moulard@epita.fr>
2019-10-05 01:04:14 +0200

    lists: adding a little more coverage

--- api/list/list_test.go
+++ api/list/list_test.go
@@ -20,20 +20,27 @@ var (
 )
 
 func TestAddList(t *testing.T) {
+	j, _ := json.Marshal(data.ListTest)
+	l := string(j)
 	tests := []struct {
 		name    string
 		want    string
+		body    string
 		wantErr bool
+		useData bool
 	}{
-		{"sanity", data.ListRe, false},
+		{"sanity", data.ListRe, "{\"list\":" + l + "}", false, true},
+		{"wrong json", "5.0.0", "}", true, false},
+		{"wrong data", "5.0.1", "{\"list\":" + l + "}", true, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "wrong data" {
+				data.Error = data.ErrTest
+			}
 			data.List = data.ListTest
-			j, _ := json.Marshal(data.ListTest)
-			body := fmt.Sprintf("{\"list\":%s}", j)
 			req, _ := http.NewRequest("POST", "/list/create",
-				strings.NewReader(body))
+				strings.NewReader(tt.body))
 			req.Header.Set("Cookie", fmt.Sprintf("token=%s", token))
 			got, err := list.AddList(d, req)
 			if (err != nil) != tt.wantErr {
@@ -47,10 +54,11 @@ func TestAddList(t *testing.T) {
 			if len(matchs) == 0 {
 				t.Errorf("AddList() = '%v', want %v", got, tt.want)
 			}
-			if data.CreateList == tt.wantErr {
-				t.Errorf("Data was no listd")
+			if data.CreateList != tt.useData {
+				t.Errorf("data.CreateList was not used")
 			}
 			data.CreateList = false
+			data.Error = nil
 		})
 	}
 }
@@ -62,9 +70,13 @@ func TestGetLists(t *testing.T) {
 		wantErr bool
 	}{
 		{"sanity", fmt.Sprintf("[%s]", data.ListRe), false},
+		{"wrong data", "5.1.0", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "wrong data" {
+				data.Error = data.ErrTest
+			}
 			data.List = data.ListTest
 			req, _ := http.NewRequest("POST", "/list/getall", nil)
 			req.Header.Set("Cookie", fmt.Sprintf("token=%s", token))
@@ -80,37 +92,45 @@ func TestGetLists(t *testing.T) {
 			if len(matchs) == 0 {
 				t.Errorf("GetLists() = '%v', want %v", got, tt.want)
 			}
-			if data.GetAllLists == tt.wantErr {
-				t.Errorf("Data was no listd")
+			if data.GetAllLists == false {
+				t.Errorf("data.GetAllLists was not used")
 			}
 			data.GetAllLists = false
+			data.Error = nil
 		})
 	}
 }
 
 func TestGetListContent(t *testing.T) {
+	data.ListContent = data.ListContentTest
+	j, _ := json.Marshal(data.ListTest)
+	l := string(j)
 	tests := []struct {
 		name    string
 		want    string
 		wantErr bool
+		body    string
+		useData bool
 	}{
-		{"sanity", fmt.Sprintf("[%s]", data.ListContentRe), false},
+		{"sanity", fmt.Sprintf("[%s]", data.ListContentRe), false,
+			"{\"list\":" + l + "}", true},
+		{"wrong json", "5.2.0", true, "}", false},
+		{"wrong data", "5.2.1", true, "{\"list\":" + l + "}", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			data.ListContent = data.ListContentTest
-			j, _ := json.Marshal(data.ListTest)
-			body := fmt.Sprintf("{\"list\":%s}", j)
-			t.Logf("%s", body)
+			if tt.name == "wrong data" {
+				data.Error = data.ErrTest
+			}
 			req, _ := http.NewRequest("POST", "/list/get",
-				strings.NewReader(body))
+				strings.NewReader(tt.body))
 			req.Header.Set("Cookie", fmt.Sprintf("token=%s", token))
 			got, err := list.GetListContent(d, req)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GetListContent() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetListContent() error = %v, wantErr %v",
+					err, tt.wantErr)
 				return
 			}
-			t.Logf("%+v", got)
 			if tt.wantErr {
 				got = err.Error()
 			}
@@ -118,29 +138,31 @@ func TestGetListContent(t *testing.T) {
 			if len(matchs) == 0 {
 				t.Errorf("GetListContent() = '%v', want %v", got, tt.want)
 			}
-			if data.GetListContent == tt.wantErr {
-				t.Errorf("Data was no listd")
+			if data.GetListContent != tt.useData {
+				t.Errorf("data.GetListContent was not used")
 			}
 			data.GetListContent = false
+			data.Error = nil
 		})
 	}
 }
 
 func TestAddPersonnal(t *testing.T) {
+	j, _ := json.Marshal(data.ListContentTest)
+	data.ID = data.IDTest
 	tests := []struct {
 		name    string
 		want    string
 		wantErr bool
+		body    string
 	}{
-		{"sanity", `{"done":true,"id":\d+}`, false},
+		{"sanity", `{"done":true,"id":\d+}`, false, string(j)},
+		{"wrong json", `5.3.0`, true, "test"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			data.ID = data.IDTest
-			j, _ := json.Marshal(data.ListContentTest)
-			t.Logf("%s", string(j))
 			req, _ := http.NewRequest("POST", "/list/get",
-				strings.NewReader(string(j)))
+				strings.NewReader(tt.body))
 			req.Header.Set("Cookie", fmt.Sprintf("token=%s", token))
 			got, err := list.AddPersonnal(d, req)
 			if (err != nil) != tt.wantErr {
@@ -148,7 +170,6 @@ func TestAddPersonnal(t *testing.T) {
 					err, tt.wantErr)
 				return
 			}
-			t.Logf("%+v", got)
 			if tt.wantErr {
 				got = err.Error()
 			}
