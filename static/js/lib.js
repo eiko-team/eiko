2f580ab8ad8aed88f7f228acf75593c7ec20bf9e
Tom Moulard <tom.moulard@epita.fr>
2019-10-21 12:10:11 +0200

    libjs: fixing list behavior adding a replaceListID function

--- static/js/lib.js
+++ static/js/lib.js
@@ -207,19 +207,42 @@ function removeLists() {
 }
 
 /**
- * load lists of the user. If the api fails, load lists from local storage
+ * load lists from local storage and update local storage from api.
  */
 function loadLists() {
     log("loadLists");
     var json = JSON.parse(localStorage.getItem("lists"));
-    if (json === null || json.error === undefined) { json = []; }
+    if (json === null || json.error !== undefined) { json = []; }
     json.forEach(addlist);
     POST("/list/getall", {}, (e) => {
-        if (e.error !== undefined) { e = [] }
+        if (e === null || e.error !== undefined) { e = [] }
         localStorage.setItem("lists", JSON.stringify(e));
         removeLists();
         e.forEach(addlist);
     });
+}
+
+function replaceListID(list, id) {
+    console.log("replaceListID", list, id);
+    var json = JSON.parse(localStorage.getItem("lists"));
+    if (json === null) { json = []; }
+    elt = json.filter(function(e) {
+        return e.id === list.id
+    });
+    console.log(json)
+    console.log("replaceListID", elt.length, elt);
+    if (elt.length === 0) { return; }
+    elt = elt[0];
+    json = json.filter(function(e) {
+        return e.id !== list.id
+    });
+    console.log(json)
+    elt.id = id;
+    json.push(elt);
+    console.log(json)
+    localStorage.setItem("lists", JSON.stringify(json));
+    // TODO: replace list id on html
+    // var lists = document.getElementById("dropdown-lists");
 }
 
 /**
@@ -228,15 +251,18 @@ function loadLists() {
  */
 function createList(name = "Liste de course") {
     log("createList=" + name);
-    var lists = localStorage.getItem("lists");
-    var json = [];
-    if (lists !== null) {
-        json = JSON.parse(lists);
-    }
-    json.push();
+    var list = { name, id: getNewUID() };
+    var json = JSON.parse(localStorage.getItem("lists"));
+    if (json !== null) { json = []; }
+    json.push(list);
     localStorage.setItem("lists", JSON.stringify(json));
-    addlist(name);
-    POST("/list/create", { name });
+    addlist(list);
+    POST("/list/create", { name }, function(e) {
+        console.log(e)
+        replaceListID(list, e.id);
+    }, function(e) {
+        console.log(e)
+    });
 }
 
 /**
@@ -449,6 +475,7 @@ function fillConsumables(fetch = false) {
         return a.ID - b.ID;
     });
     json.forEach(function(element) {
+        console.log(element.list_id === list.id, element.list_id, list.id);
         if (element.list_id === list.id) {
             hideConsumable(element.ID);
             if (element.Done) {
@@ -494,7 +521,7 @@ function goBackList(fragment = "") {
     }
 }
 
-function promiseNofity(notif){
+function promiseNofity(notif) {
     new Promise(function(resolve, reject) {
         setTimeout(function(e) { resolve() }, notif.when)
     }).then(function(event) {
@@ -524,7 +551,7 @@ function registerNotifications() {
     notifications.forEach(promiseNofity);
 }
 
-function toastMe(t){
+function toastMe(t) {
     var toasts = JSON.parse(localStorage.getItem("toasts"));
     if (toasts === null) { return; }
     toasts = toasts.filter(function(n) {
@@ -563,7 +590,7 @@ function init() {
 }
 
 function mobileAndTabletcheck() {
-  var check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
+    var check = false;
+    (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
 };
