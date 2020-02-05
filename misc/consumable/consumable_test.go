baa9163d6adaf8339ae06cfe477cdb95ed094789
Tom Moulard <tom.moulard@epita.fr>
2020-02-05 11:37:13 +0100

    consumable_test: adding more edge cases to test boycott function

--- misc/consumable/consumable_test.go
+++ misc/consumable/consumable_test.go
@@ -16,17 +16,20 @@ func TestIsBoycotted(t *testing.T) {
 		user   structures.User
 		result bool
 	}{
-		{"sanity", structures.Consumable{Company: "Test"},
-			structures.User{SBoycott: []string{"Test"}}, true},
+		{"sanity", structures.Consumable{}, structures.User{}, false},
 		{"true", structures.Consumable{Company: "Test"},
 			structures.User{SBoycott: []string{"Test"}}, true},
 		{"false", structures.Consumable{Company: "Test"},
 			structures.User{SBoycott: []string{"not Test"}}, false},
+		{"empty boycott array", structures.Consumable{Company: "Test"},
+			structures.User{}, false},
+		{"empty company", structures.Consumable{},
+			structures.User{SBoycott: []string{"Test"}}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if tt.result != consumable.IsBoycotted(tt.c, tt.user) {
-				t.Errorf("IsBoycotted(%v, %v) got %t want %t",
+				t.Errorf("IsBoycotted(%+v, %+v) got %t want %t",
 					tt.c, tt.user, consumable.IsBoycotted(tt.c, tt.user), tt.result)
 			}
 		})
